dd3ff9989305adfca94155f53d26b6e92bd9b391
Nathan Hopkins <natehop@gmail.com>
2013-10-15 09:48:54 -0600

    Changed the docker install method.

--- provision.sh
+++ provision.sh
@@ -4,4 +4,6 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list
 apt-get -qq update
-apt-get -y --force-yes install lxc-docker
+#apt-get -y --force-yes install lxc-docker
+curl http://get.docker.io | sh
+
